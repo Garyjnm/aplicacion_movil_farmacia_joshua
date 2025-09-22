6eaf202a32acf6727b2647190b4a49843c6564a9
ReneUnan <rene.sandoval23908226@estu.unan.edu.ni>
2025-09-21 20:52:00 -0600

    agregando tema claro de prueba

---
+++ lib/themes/light_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final ThemeData lightTheme = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue, brightness: Brightness.light),
+  useMaterial3: true,
+);
