c1ca9c9ee8bb7825d44b3c1968ac3e4d79e474af
ReneUnan <rene.sandoval23908226@estu.unan.edu.ni>
2025-09-21 21:09:00 -0600

    agregando los primeros elementos del tema claro de la farmacia

--- lib/themes/light_theme.dart
+++ lib/themes/light_theme.dart
@@ -2,6 +2,15 @@ import 'package:flutter/material.dart';
 
 final ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
-  colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue, brightness: Brightness.light),
-  useMaterial3: true,
+  colorScheme: const ColorScheme(
+    brightness: Brightness.light, 
+    primary: Color(0xFFBED6E3), 
+    onPrimary: Color(0xFF1B194B), 
+    secondary: Color(0xFFA3BECD), 
+    onSecondary: Color(0xFF1B194B), 
+    error: Color(0xFFE6F2F9 ), 
+    onError: Color(0xFF4D0A0F), 
+    surface: Color(0xFFF4F4F4), 
+    onSurface: Colors.black,
+    )
 );
