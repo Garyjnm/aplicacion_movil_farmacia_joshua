b79de39b96a833ce8463900db1daab1413087271
ReneUnan <rene.sandoval23908226@estu.unan.edu.ni>
2025-09-21 20:52:33 -0600

    agregando al main un boton para cambiar entre temas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'themes/light_theme.dart';
+import 'themes/dark_theme.dart';
 import 'sidebar_widget.dart';
 // import 'theme_data.dart';
 
@@ -6,25 +8,59 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.light; 
+
+  void _toggleTheme() {
+    setState(() {
+      if (_themeMode == ThemeMode.light) {
+        _themeMode = ThemeMode.dark;
+      } else {
+        _themeMode = ThemeMode.light;
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: ThemeData.dark(), home: const HomeScreen());
+    return MaterialApp(
+      title: 'Demo Farmacia Joshua',
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: _themeMode,
+      home: HomeScreen(onToggleTheme: _toggleTheme),
+    );
   }
 }
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  final VoidCallback onToggleTheme;
+  const HomeScreen({super.key, required this.onToggleTheme});
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      // drawer: const SidebarWidget(), // <-- Aquí referenciamos el sidebar
-      body: const Center(
-        child: Text('FARMACIA JOSHUA', style: TextStyle(fontSize: 20)),
+      appBar: AppBar(
+        title: const Text("Home"),
+        actions: [
+          IconButton(
+            icon: Icon(isDark ? Icons.light_mode : Icons.dark_mode),
+            onPressed: onToggleTheme,
+            tooltip: "Cambiar tema",
+          )
+        ],
+      ),
+      body: Center(
       ),
     );
   }
