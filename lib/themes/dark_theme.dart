5a908d637f02059b500a48f8e5571c3cf5e7e3e3
ReneUnan <rene.sandoval23908226@estu.unan.edu.ni>
2025-09-22 12:12:54 -0600

    modificando el tema oscuro, aun no del todo listo

--- lib/themes/dark_theme.dart
+++ lib/themes/dark_theme.dart
@@ -2,6 +2,16 @@ import 'package:flutter/material.dart';
 
 final ThemeData darkTheme = ThemeData(
   brightness: Brightness.dark,
-  colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFA3BECD), brightness: Brightness.dark),
-  useMaterial3: true,
+  colorScheme: const ColorScheme(
+    brightness: Brightness.dark, 
+    primary: Color(0xFF1B194B), 
+    onPrimary: Color(0xFF1B194B), 
+    secondary: Color(0xFFA3BECD), 
+    onSecondary: Color(0xFF1B194B), 
+    error: Color(0xFFE6F2F9 ), 
+    onError: Color(0xFF4D0A0F), 
+    surface: Color(0xFF040325), 
+    onSurface: Colors.white,
+    ),
+  useMaterial3: true
 );
