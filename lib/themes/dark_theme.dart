d6c50565175ecc2bd8363c8dc4341293daaef7f7
ReneUnan <rene.sandoval23908226@estu.unan.edu.ni>
2025-09-22 10:35:27 -0600

    agregando colores de la farmacia al tema claro

--- lib/themes/dark_theme.dart
+++ lib/themes/dark_theme.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 final ThemeData darkTheme = ThemeData(
   brightness: Brightness.dark,
-  colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue, brightness: Brightness.dark),
+  colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFA3BECD), brightness: Brightness.dark),
   useMaterial3: true,
 );
