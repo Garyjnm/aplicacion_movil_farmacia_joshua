a0fe8dc7904c3aa9258a7965103115ae7d0625e0
ReneUnan <rene.sandoval23908226@estu.unan.edu.ni>
2025-09-24 17:08:19 -0600

    mejorando la estructura del sidebar, ajustando el diseño del encabezado y los elementos de la lista

--- lib/sidebar_widget.dart
+++ lib/sidebar_widget.dart
@@ -1,3 +1,4 @@
+import 'package:aplicacion_movil_farmacia_joshua/test.dart';
 import 'package:flutter/material.dart';
 
 class SidebarWidget extends StatelessWidget {
@@ -13,25 +14,51 @@ class SidebarWidget extends StatelessWidget {
         children: [
           //Header del side bar
           DrawerHeader(
-            decoration: BoxDecoration(
-              color: colors.primary
-            ),
+            decoration: BoxDecoration(color: colors.primary),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const CircleAvatar( //avatar de prueba
+                const CircleAvatar(
+                  //avatar de prueba
                   radius: 30,
                   backgroundImage: AssetImage('assets/images/gary.jpg'),
                 ),
                 const SizedBox(height: 10),
-                Text(
-                  'Rene Alexander',
-                  //TODO
-                )
-              ],)
-          )
+                Text('Rene Alexander', style: fonts.bodyLarge),
+                Text('renealexander@gmail.com', style: fonts.bodyMedium),
+              ],
+            ),
+          ),
+          //Elementos del sidebar
+          //Home
+          ListTile(
+            leading: Icon(Icons.home, color: colors.onPrimary),
+            title: Text("Home"),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => const PlaceholderScreen(title: "Prueba"),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.receipt_long, color: colors.onPrimary),
+            title: Text("Ventas"),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => const PlaceholderScreen(title: "Ventas"),
+                ),
+              );
+            },
+          ),
         ],
-      )
+      ),
     );
   }
 }
