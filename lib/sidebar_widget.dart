3eb63ac8c51c78d0af58cddb474358d5ce81a22a
ReneUnan <rene.sandoval23908226@estu.unan.edu.ni>
2025-09-24 11:50:03 -0600

    agregando estructura del sidebar con encabezado y avatar, aun falta completar

--- lib/sidebar_widget.dart
+++ lib/sidebar_widget.dart
@@ -5,8 +5,33 @@ class SidebarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    final fonts = Theme.of(context).textTheme;
+
     return Drawer(
-      
+      child: ListView(
+        children: [
+          //Header del side bar
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: colors.primary
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CircleAvatar( //avatar de prueba
+                  radius: 30,
+                  backgroundImage: AssetImage('assets/images/gary.jpg'),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'Rene Alexander',
+                  //TODO
+                )
+              ],)
+          )
+        ],
+      )
     );
   }
 }
