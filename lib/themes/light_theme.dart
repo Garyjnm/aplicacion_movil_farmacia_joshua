8d9f2a12398eab36e61ed779ec1f751343554daf
ReneUnan <rene.sandoval23908226@estu.unan.edu.ni>
2025-09-24 11:06:27 -0600

    agregando colores terciarios al esquema de color del tema claro

--- lib/themes/light_theme.dart
+++ lib/themes/light_theme.dart
@@ -8,6 +8,8 @@ final ThemeData lightTheme = ThemeData(
     onPrimary: Color(0xFF1B194B), 
     secondary: Color(0xFFA3BECD), 
     onSecondary: Color(0xFF1B194B), 
+    tertiary: Color(0xFF6D8D9F),
+    onTertiary: Color(0xFFF4F4F4) ,
     error: Color(0xFFE6F2F9 ), 
     onError: Color(0xFF4D0A0F), 
     surface: Color(0xFFF4F4F4), 
