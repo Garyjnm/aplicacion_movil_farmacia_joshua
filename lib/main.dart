4f8599f3be2d30cb8d8c5abe2e2b49217f837509
ReneUnan <rene.sandoval23908226@estu.unan.edu.ni>
2025-09-25 21:01:03 -0600

    Estructurando la base del codigo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'themes/light_theme.dart';
-import 'themes/dark_theme.dart';
+import 'ui/core/themes/light_theme.dart';
+import 'ui/core/themes/dark_theme.dart';
 import 'sidebar_widget.dart';
 // import 'theme_data.dart';
 
