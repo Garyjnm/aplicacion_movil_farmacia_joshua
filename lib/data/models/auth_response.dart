c955d784c956061b630fcdf327fae2e789e5c3a5
Garyjnm <garyn7674@gmail.com>
2025-10-03 13:48:29 -0600

    Agregando algunos comentarios para el futuro

--- lib/data/models/auth_response.dart
+++ lib/data/models/auth_response.dart
@@ -1,4 +1,4 @@
-class AuthResponse {
+class AuthResponse { // Modelo para la respuesta de autenticación
     final String token;
     final int idUsuario;
     final String nombres;
@@ -6,7 +6,7 @@ class AuthResponse {
     final String nombreUsuario;
     final int idRol;
 
-    AuthResponse({
+    AuthResponse({ // Constructor con parámetros nombrados
         required this.token,
         required this.idUsuario,
         required this.nombres,
@@ -16,10 +16,10 @@ class AuthResponse {
         
         });
     
-    factory AuthResponse.fromJson(Map<String, dynamic> json) {
+    factory AuthResponse.fromJson(Map<String, dynamic> json) { // Método factory para crear una instancia desde JSON 
         return AuthResponse(
-            token: json['token'] ?? '',
-            idUsuario: json['idUsuario'] ?? 0,
+            token: json['token'] ?? '', //Utilizamos el operador logico por si nos llega vacio el campo
+            idUsuario: json['idUsuario'] ?? 0, //Utilizamos el operador logico por si nos llega vacio el campo
             nombres: json['nombres'] ?? '',
             apellidos: json['apellidos'] ?? '',
             nombreUsuario: json['nombreUsuario'] ?? '',
