816bfd363ed53296788be69eff4ece33dfc4eda5
Garyjnm <garyn7674@gmail.com>
2025-10-06 18:10:41 -0600

    Modificando el archivo para agregar en las rutas la pantalla de home

--- lib/ui/core/routes/routes.dart
+++ lib/ui/core/routes/routes.dart
@@ -1,12 +1,14 @@
+import 'package:aplicacion_movil_farmacia_joshua/ui/authentication/screens/home_page.dart';
 import 'package:aplicacion_movil_farmacia_joshua/ui/authentication/screens/login_screen.dart';
 import 'package:auto_route/auto_route.dart';
 import '../routes/routes.gr.dart';
 
-@AutoRouterConfig(replaceInRouteName: 'Screen|page,Route')
+@AutoRouterConfig(replaceInRouteName: 'Screen|page|Page,Route')
 class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
         AutoRoute(page: LoginRoute.page, path: LoginScreen.routeName, initial: true),
+        AutoRoute(page: HomeRoute.page, path: HomePage.routeName),
       ];
  
 }
