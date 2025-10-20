c1721b7704d0a59bf776d031cf9f074afb37e344
kenner0509 <144177320+kenner0509@users.noreply.github.com>
2025-10-20 10:47:15 -0600

    creacion del service de ventas

---
+++ lib/data/services/venta_service.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+import '../models/venta.dart';
+
+class VentaService {
+  final Dio _dio = Dio(BaseOptions(baseUrl: 'https://localhost:50498/api/'));
+
+  Future<List<Venta>> fetchVentas() async {
+    final response = await _dio.get('/venta');
+    final List data = response.data;
+    return data.map((e) => Venta.fromJson(e)).toList();
+  }
+
+  Future<Venta> fetchDetalle(int id) async {
+    final response = await _dio.get('/venta/$id');
+    return Venta.fromJson(response.data);
+  }
+}
