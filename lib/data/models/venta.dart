bfd74dae6923dcfb1bb3d8eb700ed573133f3766
kenner0509 <144177320+kenner0509@users.noreply.github.com>
2025-10-20 10:42:12 -0600

    creacion de modelo de maestro detalle venta

---
+++ lib/data/models/venta.dart
@@ -0,0 +1,37 @@
+import 'detalle_venta.dart';
+
+class Venta {
+  final int idVenta;
+  final int idCliente;
+  final int idUsuario;
+  final DateTime fechaVenta;
+  final double total;
+  final List<DetalleVenta> ventaDetalle;
+
+  Venta({
+    required this.idVenta,
+    required this.idCliente,
+    required this.idUsuario,
+    required this.fechaVenta,
+    required this.total,
+    required this.ventaDetalle,
+  });
+
+  factory Venta.fromJson(Map<String, dynamic> json) {
+    var detalleList = <DetalleVenta>[];
+    if (json['ventaDetalle'] != null) {
+      detalleList = List<Map<String, dynamic>>.from(json['ventaDetalle'])
+          .map((e) => DetalleVenta.fromJson(e))
+          .toList();
+    }
+
+    return Venta(
+      idVenta: json['idVenta'],
+      idCliente: json['idCliente'],
+      idUsuario: json['idUsuario'],
+      fechaVenta: DateTime.parse(json['fechaVenta']),
+      total: (json['total'] as num).toDouble(),
+      ventaDetalle: detalleList,
+    );
+  }
+}
