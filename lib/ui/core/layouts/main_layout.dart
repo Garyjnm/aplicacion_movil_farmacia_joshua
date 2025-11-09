124f46a282c76de5ba98e469928c4cd34cd6baf5
Garyjnm <garyn7674@gmail.com>
2025-11-09 17:07:16 -0600

    Creando el archivo auth_guar este contendra la logica de que si contenemos el token activo nos mantendremos en la pantalla que estabamos y no nos pedira volver a iniciar sesion pero si no tenemos ningun token nos va a redirigir a la pantalla login

--- lib/ui/core/layouts/main_layout.dart
+++ lib/ui/core/layouts/main_layout.dart
@@ -1,3 +1,19 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:aplicacion_movil_farmacia_joshua/sidebar_widget.dart';
+
+@RoutePage()
+class MainLayout extends StatelessWidget {
+  const MainLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const SidebarWidget(),
+      appBar: AppBar(
+        title: const Text('Farmacia Joshua'),
+      ),
+      body: const AutoRouter(),
+    );
+  }
+}
