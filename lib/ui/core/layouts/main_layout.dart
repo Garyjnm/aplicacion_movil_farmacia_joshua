411deebe1a4927c71a2d17e964655d1e16e0136f
Garyjnm <garyn7674@gmail.com>
2025-11-09 18:34:16 -0600

    Modificando el routes para que este protegido por el authGuard

--- lib/ui/core/layouts/main_layout.dart
+++ lib/ui/core/layouts/main_layout.dart
@@ -2,7 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:aplicacion_movil_farmacia_joshua/sidebar_widget.dart';
 
-@RoutePage()
+@RoutePage(name: 'MainLayoutRoute')
 class MainLayout extends StatelessWidget {
   const MainLayout({super.key});
 
